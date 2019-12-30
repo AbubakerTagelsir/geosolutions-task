938d4f09d06f72a135212f4eaa6713e9bb805ad2
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2019-12-30 16:27:06 +0400

    create point model

--- points/models.py
+++ points/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Point(models.Model):
+
+    x = models.FloatField(_("X"))
+    y = models.FloatField(_("Y"))
+    pid = models.IntegerField(_("Point ID"))
+
+    class Meta:
+        verbose_name = _("point")
+        verbose_name_plural = _("points")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("point_detail", kwargs={"pk": self.pk})
