66fd30570390f41ef97c1091c1bcfc9bd2bc8937
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2019-12-31 15:13:09 +0400

    history model

--- points/models.py
+++ points/models.py
@@ -15,7 +15,27 @@ class Point(models.Model):
         verbose_name_plural = _("points")
 
     def __str__(self):
-        return self.name
+        return self.pid
 
     def get_absolute_url(self):
         return reverse("point_detail", kwargs={"pk": self.pk})
+
+
+class History(models.Model):
+
+    x = models.FloatField(_("X"))
+    y = models.FloatField(_("Y"))
+    n = models.IntegerField(_("Number of points"))
+    operation_type = models.IntegerField(choices=((1, _("Nearest")),
+                                        (2, _("Furthest"))),
+                                default=1)
+
+    class Meta:
+        verbose_name = _("history")
+        verbose_name_plural = _("historys")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("history_detail", kwargs={"pk": self.pk})
--- points/views.py
+++ points/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Point, History
+from django.views import generic
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'points/index.html')
+
+def new_form(request):
+    return render(request, 'points/new.html')
+
+def submit_form(request):    
+    return render(request, 'points/history.html')
+
+class NewView(generic.DetailView):
+    model = Point
+    template_name = 'points/new.html'
+
+class HistoryView(generic.ListView):
+    model = Point
+    template_name = 'points/history.html'
+    # return render(request, 'points/history.html')
+
